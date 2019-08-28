c5e0e3dc79bfb2a0fc4302c5e6275fdd49c03ff1
JefferssonSemin <jeffesemin@hotmail.com>
2019-08-28 14:14:13 -0300

    adicionado modelo de base

---
+++ ModeloApiRestFull/Controllers/MonitoramentoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModeloApiRestFull.Controllers
+{
+    [Route("Monitoramento")]
+    [ApiController]
+    public class MonitoramentoController : ControllerBase
+    {
+        [HttpGet, Route("Auth"), Authorize]
+        public IActionResult Auth()
+        {
+            return Ok("Ok");
+        }
+
+        [HttpGet]
+        public IActionResult Monitoramento()
+        {
+            return Ok("MonitoramentoOK");
+        }
+    }
+}
---
+++ ModeloApiRestFull/Startup.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ModeloApiRestFull
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(AzureADB2CDefaults.BearerAuthenticationScheme)
+                .AddAzureADB2CBearer(options => Configuration.Bind("AzureAdB2C", options));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Modelo restfull api",
+                    Description = "Modelo de api restfull"
+                });
+                s.AddSecurityDefinition("Bearer", new ApiKeyScheme { In = "header", Description = "Insira o seu Token JWT aqui!", Name = "Authorization", Type = "apiKey" });
+                s.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>> {
+                    { "Bearer", Enumerable.Empty<string>() },
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("../swagger/v1/swagger.json", "Projeto v1.0");
+            });
+            app.UseRewriter(new RewriteOptions().AddRedirect("^$", "swagger"));
+        }
+    }
+}
